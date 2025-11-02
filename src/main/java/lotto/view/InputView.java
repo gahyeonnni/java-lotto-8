d33c13fb768c74ecb0fff95cad7820d48bd096dc
김가현 <hyeonnni@naver.com>
2025-11-02 20:54:24 +0900

    feat(view): 구입 금액, 당첨 번호, 보너스 번호 입력 기능 추가

---
+++ src/main/java/lotto/view/InputView.java
@@ -0,0 +1,22 @@
+package lotto.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import lotto.message.IOMessage;
+
+public class InputView {
+
+    public String getAmount() {
+        System.out.println(IOMessage.ASK_PURCHASE_AMOUNT.message());
+        return Console.readLine();
+    }
+
+    public String getWinningNumber() {
+        System.out.println(IOMessage.ASK_WINNING_NUMBERS.message());
+        return Console.readLine();
+    }
+
+    public String getBonusNumber() {
+        System.out.println(IOMessage.ASK_BONUS_NUMBER.message());
+        return Console.readLine();
+    }
+}
