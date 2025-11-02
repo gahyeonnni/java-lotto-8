36ad19afe813cdeda34cf9eba132e6ad9f047141
김가현 <hyeonnni@naver.com>
2025-11-02 21:37:16 +0900

    fix(lotto): ArrayList로 변환해 정렬 가능하게 변경

--- src/main/java/lotto/lotto/LottoNumberGenerator.java
+++ src/main/java/lotto/lotto/LottoNumberGenerator.java
@@ -2,13 +2,14 @@ package lotto.lotto;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class LottoNumberGenerator implements NumberGenerator {
     @Override
     public List <Integer> generate(int min, int max, int count) {
-        List <Integer> lotto = Randoms.pickUniqueNumbersInRange(min, max, count);
+        List<Integer> lotto = new ArrayList<>(Randoms.pickUniqueNumbersInRange(min, max, count));
         Collections.sort(lotto);
         return lotto;
     }
