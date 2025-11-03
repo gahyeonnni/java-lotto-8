1ec8563cddc3dc6aec2ae4e10a348201f8cf5faa
김가현 <hyeonnni@naver.com>
2025-11-03 17:43:21 +0900

    feat(message): 에러 메시지 상수 추가

--- src/main/java/lotto/message/ErrorMessage.java
+++ src/main/java/lotto/message/ErrorMessage.java
@@ -7,6 +7,7 @@ public enum ErrorMessage {
     NON_NUMERIC("[ERROR] 숫자가 아닙니다."),
     COUNT_OUT_OF_RANGE("[ERROR] 범위를 초과했습니다."),
     NUMBER_OUT_OF_RANGE("[ERROR] 로또 번호는 1부터 45 사이여야 합니다."),
+    SIZE_OF_WINNING_NUMBER("[ERROR] 로또 번호는 6개여야 합니다."),
     DUPLICATE_BONUS_NUMBER("[ERROR] 보너스 번호는 당첨 번호와 중복될 수 없습니다."),
     DUPLICATE_WINNING_NUMBER("[ERROR] 당첨 번호는 중복될 수 없습니다.");
 
