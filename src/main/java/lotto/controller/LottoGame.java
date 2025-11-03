11466d031cee992ee65f547608b81b47fc9bc65b
김가현 <hyeonnni@naver.com>
2025-11-03 17:40:11 +0900

    feat(controller): 로또 게임 실행 로직 구현

--- src/main/java/lotto/controller/LottoGame.java
+++ src/main/java/lotto/controller/LottoGame.java
@@ -1,60 +1,39 @@
+// lotto/controller/LottoGame.java
 package lotto.controller;
 
 import lotto.domain.Lotto;
 import lotto.domain.LottoRank;
 import lotto.domain.WinningLotto;
-import lotto.view.InputView;
+import lotto.service.InputService;
+import lotto.service.LottoService;
 import lotto.view.OutputView;
-import lotto.lotto.LottoManager;
-import lotto.util.InputValidator;
 
 import java.util.List;
 import java.util.Map;
 
 public class LottoGame {
-    private final InputView inputView;
+    private final InputService inputService;
     private final OutputView outputView;
-    private final InputValidator validator;
-    private final LottoManager lottoManager;
+    private final LottoService lottoService;
 
-    public LottoGame(
-            InputView inputView,
-            OutputView outputView,
-            InputValidator validator,
-            LottoManager lottoManager
-    ) {
-        this.inputView = inputView;
+    public LottoGame(InputService inputService, OutputView outputView, LottoService lottoService) {
+        this.inputService = inputService;
         this.outputView = outputView;
-        this.validator = validator;
-        this.lottoManager = lottoManager;
+        this.lottoService = lottoService;
     }
 
     public void run() {
         try {
-            int purchaseCount = getPurchaseCount();
-            List<Lotto> purchasedLottos = lottoManager.createLottos(purchaseCount);
-            outputView.printPurchasedLotto(purchasedLottos);
-            WinningLotto winningLotto = getWinningLotto();
-            Map<LottoRank, Integer> results = lottoManager.checkResults(purchasedLottos, winningLotto);
-            double rate = lottoManager.calculateProfitRate(results, purchaseCount * 1000);
-            outputView.printWinningStatistics(results, rate);
+            int purchaseCount = inputService.getPurchaseCount();
+            List<Lotto> purchased = lottoService.createLottos(purchaseCount);
+            WinningLotto winning = inputService.getWinningLotto();
+            Map<LottoRank, Integer> result = lottoService.checkResults(purchased, winning);
+            double rate = lottoService.calculateProfitRate(result, purchaseCount * 1000);
+            outputView.printWinningStatistics(result, rate);
         }
         catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             run();
         }
-    }
-
-    private int getPurchaseCount() {
-        String amountInput = inputView.getAmount();
-        validator.validatePurchaseAmount(amountInput);
-        return Integer.parseInt(amountInput) / 1000;
-    }
-
-    private WinningLotto getWinningLotto() {
-        List<Integer> winningNumbers = validator.parseLottoInput(inputView.getWinningNumber());
-        int bonus = validator.parseBonusInput(inputView.getBonusNumber());
-        validator.validateBonusNumber(bonus, winningNumbers);
-        return new WinningLotto(new Lotto(winningNumbers), bonus);
     }
 }
