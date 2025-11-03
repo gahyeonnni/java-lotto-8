5e5e707edf72bae046e920b518aaa0c90a9ae5ca
김가현 <hyeonnni@naver.com>
2025-11-03 17:41:05 +0900

    refactor(domain): Lotto를 불변 리스트로 변경

--- src/main/java/lotto/domain/Lotto.java
+++ src/main/java/lotto/domain/Lotto.java
@@ -6,24 +6,33 @@ import java.util.List;
 import java.util.Set;
 
 public class Lotto {
-    private final List<Integer> numbers;
+    private static final int LOTTO_SIZE = 6;
+    private final List<LottoNumber> numbers;
 
-    public Lotto(List<Integer> numbers) {
+    public Lotto(List<LottoNumber> numbers) {
         validate(numbers);
         this.numbers = List.copyOf(numbers);
     }
 
-    private void validate(List<Integer> numbers) {
-        if (numbers.size() != 6) {
-            throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
+    private void validate(List<LottoNumber> numbers) {
+        validateSize(numbers);
+        validateUnique(numbers);
+    }
+
+    private void validateSize(List<LottoNumber> numbers) {
+        if (numbers.size() != LOTTO_SIZE) {
+            throw new IllegalArgumentException(ErrorMessage.SIZE_OF_WINNING_NUMBER.message());
         }
-        Set<Integer> unique = new HashSet<>(numbers);
-        if (unique.size() != 6) {
+    }
+
+    private void validateUnique(List<LottoNumber> numbers) {
+        Set<LottoNumber> unique = new HashSet<>(numbers);
+        if (unique.size() != LOTTO_SIZE) {
             throw new IllegalArgumentException(ErrorMessage.DUPLICATE_WINNING_NUMBER.message());
         }
     }
 
-    public List<Integer> getNumbers() {
-        return numbers;
+    public List<LottoNumber> getNumbers() {
+        return List.copyOf(numbers);
     }
 }
