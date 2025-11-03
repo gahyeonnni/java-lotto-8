3320f88f693aa389094191021444354b420b1029
김가현 <hyeonnni@naver.com>
2025-11-03 17:39:27 +0900

    chore(controller): LottoFactory 의존성 생성

--- src/main/java/lotto/controller/LottoFactory.java
+++ src/main/java/lotto/controller/LottoFactory.java
@@ -1,21 +1,22 @@
 package lotto.controller;
 
+import lotto.service.InputService;
+import lotto.service.LottoService;
 import lotto.view.InputView;
 import lotto.view.OutputView;
-import lotto.lotto.LottoManager;
-import lotto.lotto.LottoNumberGenerator;
-import lotto.lotto.NumberGenerator;
+import lotto.generator.LottoNumberGenerator;
+import lotto.generator.NumberGenerator;
 import lotto.util.InputValidator;
 
-//Factory 로 이름을 바꿔라
-public class Config {
+public class LottoFactory {
     public LottoGame createGame() {
         InputValidator validator = new InputValidator();
         InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-        NumberGenerator numberGenerator = new LottoNumberGenerator();
-        LottoManager lottoManager = new LottoManager(numberGenerator);
+        NumberGenerator generator = new LottoNumberGenerator();
+        LottoService lottoService = new LottoService(generator, outputView);
+        InputService inputService = new InputService(inputView, validator);
 
-        return new LottoGame(inputView, outputView, validator, lottoManager);
+        return new LottoGame(inputService, outputView, lottoService);
     }
 }
