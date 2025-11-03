31db7e05f78c3aba8b16c8339aee4f87d2579485
김가현 <hyeonnni@naver.com>
2025-11-03 17:46:00 +0900

    feat(service): 사용자 입력값을 검증하고 객체로 변환하는 기능 구현

---
+++ src/main/java/lotto/service/InputService.java
@@ -0,0 +1,35 @@
+package lotto.service;
+
+import lotto.domain.Lotto;
+import lotto.domain.LottoNumber;
+import lotto.domain.WinningLotto;
+import lotto.util.InputValidator;
+import lotto.view.InputView;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InputService {
+    private final InputView inputView;
+    private final InputValidator validator;
+
+    public InputService(InputView inputView, InputValidator validator) {
+        this.inputView = inputView;
+        this.validator = validator;
+    }
+
+    public int getPurchaseCount() {
+        String input = inputView.getAmount();
+        validator.validatePurchaseAmount(input);
+        return Integer.parseInt(input) / 1000;
+    }
+
+    public WinningLotto getWinningLotto() {
+        List<Integer> inputNumbers = validator.parseLottoInput(inputView.getWinningNumber());
+        int bonus = validator.parseBonusInput(inputView.getBonusNumber());
+        List<LottoNumber> numbers = inputNumbers.stream()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
+        return new WinningLotto(new Lotto(numbers), bonus);
+    }
+}
