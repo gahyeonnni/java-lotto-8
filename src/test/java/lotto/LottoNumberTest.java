211bc7f33dee6034bd076f94f927ffc4a4df3aee
김가현 <hyeonnni@naver.com>
2025-11-03 19:50:27 +0900

    chore(test): 패키지 경로 수정

--- src/test/java/lotto/LottoNumberTest.java
+++ src/test/java/lotto/LottoNumberTest.java
@@ -1,5 +1,6 @@
-package lotto.domain;
+package lotto;
 
+import lotto.domain.LottoNumber;
 import lotto.message.ErrorMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
